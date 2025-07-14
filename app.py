463ef7bf40d69df3795522fa3db2aaf7fd6398df
Vibhaw Kureel <132392947+rambo1111@users.noreply.github.com>
2025-07-14 15:16:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
 
 # Initialize model (Gemini 1.5 Flash)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.5-flash')
 
 # Flask app
 app = Flask(__name__)
